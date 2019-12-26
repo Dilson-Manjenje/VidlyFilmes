7ce449f9a51c1ab785af121937491b6b7717ba46
dilson.manjenje <dilson.manjenje@outlook.com>
2019-12-26 08:14:07 +0100

    WebApi

---
+++ Vidly/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Vidly
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",                
+                defaults: new { controller = "Clientes", action = "Index", id = RouteParameter.Optional }
+                //defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Controllers/Api/ClientesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers.Api
+{
+    public class ClientesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ClientesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            //base.Dispose(disposing);
+            _dbContext.Dispose();
+        }
+
+        // GET  /api/Clientes/
+        public IEnumerable<Cliente> Index()
+        {
+            
+            var clientes = _dbContext.Clientes.Include(c => c.TipoSubscricao)
+                                              .OrderBy(c => c.Nome)
+                                              .ToList();
+
+            return clientes;
+        }
+
+        // GET  /api/Clientes/id
+        public Cliente GetCliente(Guid id)
+        {
+            if ( id == Guid.Empty )
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                            
+            var cliente = _dbContext.Clientes.Include(c => c.TipoSubscricao).SingleOrDefault(c => c.Id == id);
+
+            if ( cliente == null )
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return cliente;
+        }
+    }
+}
